fce90046e879899f0a1a78ba2a711604b070d7e0
itumelengkolwane <162047181+katherinetumi@users.noreply.github.com>
2024-03-27 22:00:19 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export default function App() {
             open-sourced on GitHub
           </a>{" "}
           and{" "}
-          <a href="#" target="_blank" rel="noopener noreferrer">
+          <a href="https://react-weather-app-nine-sandy.vercel.app/" target="_blank" rel="noopener noreferrer">
             hosted on Netlify
           </a>
         </footer>
